10138651fc2f4fde47ec3ce0a5037478463301fd
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-11-24 23:35:03 +0700

    Add app modules w/o auth with modularization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fooody/drawer.dart';
+import 'package:fooody/widgets/drawer.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ send_foodwaste/lib/send_foodwaste.dart
@@ -0,0 +1,7 @@
+library send_foodwaste;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}

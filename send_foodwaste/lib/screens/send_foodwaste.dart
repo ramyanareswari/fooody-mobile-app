c217587706bf4ebc8dc92e26abbbc9d85093cc3d
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-12-03 00:17:02 +0700

    Added form (not finished)

--- send_foodwaste/lib/screens/send_foodwaste.dart
+++ send_foodwaste/lib/screens/send_foodwaste.dart
@@ -3,59 +3,128 @@
 import 'package:flutter/material.dart';
 import 'package:fooody/widgets/drawer.dart';
 
-
 class SendFoodHomePage extends StatefulWidget {
   const SendFoodHomePage({Key? key}) : super(key: key);
 
-  
   @override
   State<SendFoodHomePage> createState() => _SendFood_HomePageState();
 }
 
-
 class _SendFood_HomePageState extends State<SendFoodHomePage> {
-  int _counter = 0;
-
-  // ignore: unused_element
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
+  final _formKey = GlobalKey<FormState>();
+  String name = "";
+  String address = "";
+  String phoneNumber = "";
+  DateTime expiryDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
-      appBar: AppBar(
-      
-        title: const Text("Send Foodwaste Home Page"),
-      ),
-      drawer: const AppDrawer(),
-      body: Center(
-        
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text("Send Foodwaste Home Page"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFFFEA150),
-        onPressed: () {
-        },
-        tooltip: 'Tambah Sisa Makanan',
-        child: const Icon(Icons.add),
-      ), 
-    );
+        drawer: const AppDrawer(),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          labelText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        onChanged: (String? value) {
+                          setState(() {
+                            name = value!;
+                          });
+                        },
+                        onSaved: (String? value) {
+                          setState(() {
+                            name = value!;
+                          });
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Address',
+                          labelText: 'Address',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        onChanged: (String? value) {
+                          setState(() {
+                            address = value!;
+                          });
+                        },
+                        onSaved: (String? value) {
+                          setState(() {
+                            address = value!;
+                          });
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an address';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Phone number',
+                          labelText: 'Phone number',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        onChanged: (String? value) {
+                          setState(() {
+                            phoneNumber = value!;
+                          });
+                        },
+                        onSaved: (String? value) {
+                          setState(() {
+                            phoneNumber = value!;
+                          });
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a phone number';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InputDatePickerFormField(
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2100),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ));
   }
 }
---
+++ send_foodwaste/util/fetch_send_foodwaste.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:send_foodwaste/models/send_foodwaste_model.dart';
+
+Future<List<SendFoodWasteModel>> fetchMyWatchList() async {
+    var url =
+        Uri.parse('https://fooodybuddy.up.railway.app/send_foodwaste/json/');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object ToDo
+    List<SendFoodWasteModel> listFoodWaste = [];
+    for (var d in data) {
+      if (d != null) {
+        listFoodWaste.add(SendFoodWasteModel.fromJson(d));
+      }
+    }
+
+    return listFoodWaste;
+  }

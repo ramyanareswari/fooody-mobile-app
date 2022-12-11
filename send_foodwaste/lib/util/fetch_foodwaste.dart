2ba0369719d47cfe9c4c5d8e3065cc6dc15a1499
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-12-11 13:59:26 +0700

    Added data fetching

--- send_foodwaste/lib/util/fetch_foodwaste.dart
+++ send_foodwaste/lib/util/fetch_foodwaste.dart
@@ -1,8 +1,9 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:send_foodwaste/models/send_foodwaste_model.dart';
+import 'package:fooody/common/cookie_request.dart';
 
-Future<List<SendFoodWasteModel>> fetchMyWatchList() async {
+Future<List<SendFoodWasteModel>> fetchFoodWaste(CookieRequest request) async {
     var url =
         Uri.parse('https://fooodybuddy.up.railway.app/send_foodwaste/json/');
     var response = await http.get(

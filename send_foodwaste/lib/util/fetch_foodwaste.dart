d4549f81318a8a2dfa93ce13afdf61c68a909686
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-12-12 01:53:27 +0700

    Send foodwaste : Updated data fetching

--- send_foodwaste/lib/util/fetch_foodwaste.dart
+++ send_foodwaste/lib/util/fetch_foodwaste.dart
@@ -4,26 +4,17 @@ import 'package:send_foodwaste/models/send_foodwaste_model.dart';
 import 'package:fooody/common/cookie_request.dart';
 
 Future<List<SendFoodWasteModel>> fetchFoodWaste(CookieRequest request) async {
-    var url =
-        Uri.parse('https://fooodybuddy.up.railway.app/send_foodwaste/json/');
-    var response = await http.get(
-      url,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-    );
+  var response = await request.get(
+    'https://fooodybuddy.up.railway.app/send_foodwaste/json/',
+  );
 
-    // melakukan decode response menjadi bentuk json
-    var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-    // melakukan konversi data json menjadi object ToDo
-    List<SendFoodWasteModel> listFoodWaste = [];
-    for (var d in data) {
-      if (d != null) {
-        listFoodWaste.add(SendFoodWasteModel.fromJson(d));
-      }
+  // melakukan konversi data json menjadi object ToDo
+  List<SendFoodWasteModel> listFoodWaste = [];
+  for (var d in response) {
+    if (d != null) {
+      listFoodWaste.add(SendFoodWasteModel.fromJson(d));
     }
-
-    return listFoodWaste;
   }
+
+  return listFoodWaste;
+}
